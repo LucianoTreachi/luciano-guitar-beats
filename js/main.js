415efef8e68f64d43de02cf7b79e4d6fd8840fdb
lucianotreachi <lucianotreachi@outlook.com>
2024-11-02 10:01:36 -0300

    Agregar evento 'DOMContentLoaded' para asegurar de que todos los elementos del DOM estén disponibles antes de intentar acceder a ellos o agregarles eventos

--- js/main.js
+++ js/main.js
@@ -1,329 +1,332 @@
-////////// DOM ELEMENTS ////////// 
-const header = document.querySelector("header");
+window.addEventListener('DOMContentLoaded', () => {
+  ////////// DOM ELEMENTS ////////// 
+  const header = document.querySelector("header");
 
-const openMenuButton = document.querySelector(".open-menu-button");
-const closeMenuButton = document.querySelector(".close-menu-button");
-const navbar = document.querySelector(".navbar");
-const navLinks = document.querySelectorAll('.nav-link')
+  const openMenuButton = document.querySelector(".open-menu-button");
+  const closeMenuButton = document.querySelector(".close-menu-button");
+  const navbar = document.querySelector(".navbar");
+  const navLinks = document.querySelectorAll('.nav-link')
 
-const sections = document.querySelectorAll('section')
+  const sections = document.querySelectorAll('section')
 
-const modalBg = document.querySelectorAll(".modal-bg");
-const iconModal = document.querySelectorAll(".icon");
-const imgModal = document.querySelectorAll(".img-video");
-const closeModal = document.querySelectorAll(".close-modal");
-const iframe = document.querySelectorAll("iframe");
+  const modalBg = document.querySelectorAll(".modal-bg");
+  const iconModal = document.querySelectorAll(".icon");
+  const imgModal = document.querySelectorAll(".img-video");
+  const closeModal = document.querySelectorAll(".close-modal");
+  const iframe = document.querySelectorAll("iframe");
 
-const scrollUp = document.getElementById('scroll-up')
+  const scrollUp = document.getElementById('scroll-up')
 
-////////// RESPONSIVE MENU //////////
-function openMenu() {
-  navbar.classList.add("active");
-}
-
-function closeMenu() {
-  navbar.classList.remove("active");
-}
-
-openMenuButton.addEventListener('click', openMenu);
-closeMenuButton.addEventListener('click', closeMenu);
-
-navLinks.forEach((e) => {
-  e.addEventListener('click', closeMenu);
-});
-
-////////// HEADER ////////// 
-function toggleHeaderClass() {
-  if (window.scrollY > 0) {
-    header.classList.add('active');
-  } else {
-    header.classList.remove('active');
+  ////////// RESPONSIVE MENU //////////
+  function openMenu() {
+    navbar.classList.add("active");
   }
-}
 
-window.onscroll = () => {
-  closeMenu();
-  toggleHeaderClass();
-};
+  function closeMenu() {
+    navbar.classList.remove("active");
+  }
 
-window.onload = toggleHeaderClass;
+  openMenuButton.addEventListener('click', openMenu);
+  closeMenuButton.addEventListener('click', closeMenu);
 
-////////// ACTIVE LINK //////////
-function activeLinks() {
-  const scrollPosition = window.scrollY + 97;
-  let activeIndex = -1;
+  navLinks.forEach((e) => {
+    e.addEventListener('click', closeMenu);
+  });
 
-  for (let i = 0; i < sections.length; i++) {
-    if (scrollPosition >= sections[i].offsetTop) {
-      activeIndex = i;
+  ////////// HEADER ////////// 
+  function toggleHeaderClass() {
+    if (window.scrollY > 0) {
+      header.classList.add('active');
     } else {
-      break;
+      header.classList.remove('active');
     }
   }
 
-  navLinks.forEach(link => link.classList.remove("active"));
-  if (activeIndex >= 0) {
-    navLinks[activeIndex].classList.add("active");
+  window.onscroll = () => {
+    closeMenu();
+    toggleHeaderClass();
+  };
+
+  window.onload = toggleHeaderClass;
+
+  ////////// ACTIVE LINK //////////
+  function activeLinks() {
+    const scrollPosition = window.scrollY + 97;
+    let activeIndex = -1;
+
+    for (let i = 0; i < sections.length; i++) {
+      if (scrollPosition >= sections[i].offsetTop) {
+        activeIndex = i;
+      } else {
+        break;
+      }
+    }
+
+    navLinks.forEach(link => link.classList.remove("active"));
+    if (activeIndex >= 0) {
+      navLinks[activeIndex].classList.add("active");
+    }
   }
-}
 
-window.addEventListener('load', activeLinks);
-window.addEventListener('scroll', activeLinks);
+  window.addEventListener('load', activeLinks);
+  window.addEventListener('scroll', activeLinks);
 
-////////// SCROLL TO SECTION //////////
-navLinks.forEach(link => {
-  link.addEventListener('click', (event) => {
+  ////////// SCROLL TO SECTION //////////
+  navLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+
+      const sectionId = link.getAttribute('href').substring(1);
+      const targetSection = document.getElementById(sectionId);
+
+      if (targetSection) {
+        targetSection.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        });
+      }
+    });
+  });
+
+  ////////// SCROLL UP //////////
+  window.addEventListener("scroll", function () {
+    scrollUp.classList.toggle("active", window.scrollY > 500);
+  });
+
+  scrollUp.addEventListener("click", (event) => {
     event.preventDefault();
 
-    const sectionId = link.getAttribute('href').substring(1);
-    const targetSection = document.getElementById(sectionId);
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+  })
 
-    if (targetSection) {
-      targetSection.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start'
+  ////////// MODAL ////////// 
+  let modals = function (modalClick) {
+    modalBg[modalClick].classList.add("active");
+  };
+
+  iconModal.forEach((iconPlay, e) => {
+    iconPlay.addEventListener("click", () => {
+      modals(e);
+    });
+  });
+
+  imgModal.forEach((video, e) => {
+    video.addEventListener("click", () => {
+      modals(e);
+    });
+  });
+
+  closeModal.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      modalBg.forEach((modal) => {
+        modal.classList.remove("active");
+        iframe.forEach((video) => {
+          video.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+        })
       });
+    });
+  });
+
+  modalBg.forEach((modal) => {
+    modal.addEventListener("click", () => {
+      modalBg.forEach((modal) => {
+        modal.classList.remove("active");
+        iframe.forEach((video) => {
+          video.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+        })
+      });
+    });
+  });
+
+  ////////// SWIPER BLOGS ////////// 
+  const blogs = new Swiper(".blogs-slider", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 30,
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: false,
+    },
+
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      600: {
+        slidesPerView: 2,
+      },
+      1000: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+  ////////// SWIPER REVIEWS ////////// 
+  const reviews = new Swiper(".review-slider", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 30,
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: false,
+    },
+
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      600: {
+        slidesPerView: 2,
+      },
+      1000: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+  ////////// CONTACT //////////
+  const contactSection = document.getElementById('contact')
+  const form = document.getElementById('form');
+  const inputs = document.querySelectorAll('#form input');
+
+  const inputName = document.getElementById("input-name");
+  const inputEmail = document.getElementById("input-email");
+  const textarea = document.getElementById('textarea');
+
+  const alertName = document.getElementById('alert-name');
+  const alertEmail = document.getElementById("alert-email");
+  const alertMessage = document.getElementById("alert-message");
+
+  const modalContact = document.getElementById("modal-contact");
+  const modalContactLoader = document.getElementById("modal-contact-loader");
+  const modalContactSuccess = document.getElementById("modal-contact-success");
+  const modalContactError = document.getElementById("modal-contact-error");
+  const closeModalContact = document.querySelectorAll(".close-modal-contact");
+
+  /* Regular Expressions */
+  const expName = /^[\S][\DÀ-ÿ\s]{1,30}$/;
+  const expEmail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{4,63}\.){1,125}(com|net|org|edu|gov|mil|biz|info|mobi|name|aero|asia|jobs|museum)\b$/i;
+  const expMessage = /^[\S][0-9a-zA-ZÀ-ÿ\s\,.¡!¿?]{3,1000}$/;
+
+  const campos = {
+    nombre: false,
+    correo: false,
+    mensaje: false
+  }
+
+  /* Validate Regular Expressions */
+  function validateRegularExpressions(e) {
+    switch (e.target.id) { // with e.target.name we get the name value of each input
+
+      case "input-name":
+        // test if the regular expression nombre is true in the input value (e.target.value)
+        if (expName.test(e.target.value)) {
+          alertName.classList.remove('active');
+          campos.nombre = true;
+        }
+        else {
+          alertName.classList.add('active');
+          campos.nombre = false;
+        }
+        break;
+
+      case "input-email":
+        // test if the regular expression correo is true in the input value (e.target.value)
+        if (expEmail.test(e.target.value)) {
+          alertEmail.classList.remove('active');
+          campos.correo = true;
+        }
+        else {
+          alertEmail.classList.add('active');
+          campos.correo = false;
+        }
+        break;
+
+      case "textarea":
+        // test if the regular expression mensaje is true in the input value (e.target.value)
+        if (expMessage.test(e.target.value)) {
+          alertMessage.classList.remove('active');
+          campos.mensaje = true;
+        }
+        else {
+          alertMessage.classList.add('active');
+          campos.mensaje = false;
+        }
+        break;
     }
-  });
-});
-
-////////// SCROLL UP //////////
-window.addEventListener("scroll", function () {
-  scrollUp.classList.toggle("active", window.scrollY > 500);
-});
-
-scrollUp.addEventListener("click", (event) => {
-  event.preventDefault();
-
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: 'smooth'
-  });
-})
-
-////////// MODAL ////////// 
-let modals = function (modalClick) {
-  modalBg[modalClick].classList.add("active");
-};
-
-iconModal.forEach((iconPlay, e) => {
-  iconPlay.addEventListener("click", () => {
-    modals(e);
-  });
-});
-
-imgModal.forEach((video, e) => {
-  video.addEventListener("click", () => {
-    modals(e);
-  });
-});
-
-closeModal.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    modalBg.forEach((modal) => {
-      modal.classList.remove("active");
-      iframe.forEach((video) => {
-        video.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
-      })
-    });
-  });
-});
-
-modalBg.forEach((modal) => {
-  modal.addEventListener("click", () => {
-    modalBg.forEach((modal) => {
-      modal.classList.remove("active");
-      iframe.forEach((video) => {
-        video.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
-      })
-    });
-  });
-});
-
-////////// SWIPER BLOGS ////////// 
-const blogs = new Swiper(".blogs-slider", {
-  loop: true,
-  grabCursor: true,
-  spaceBetween: 30,
-
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: false,
-  },
-
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    600: {
-      slidesPerView: 2,
-    },
-    1000: {
-      slidesPerView: 3,
-    },
-  },
-});
-
-////////// SWIPER REVIEWS ////////// 
-const reviews = new Swiper(".review-slider", {
-  loop: true,
-  grabCursor: true,
-  spaceBetween: 30,
-
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: false,
-  },
-
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    600: {
-      slidesPerView: 2,
-    },
-    1000: {
-      slidesPerView: 3,
-    },
-  },
-});
-
-////////// CONTACT //////////
-const contactSection = document.getElementById('contact')
-const form = document.getElementById('form');
-const inputs = document.querySelectorAll('#form input');
-
-const inputName = document.getElementById("input-name");
-const inputEmail = document.getElementById("input-email");
-const textarea = document.getElementById('textarea');
-
-const alertName = document.getElementById('alert-name');
-const alertEmail = document.getElementById("alert-email");
-const alertMessage = document.getElementById("alert-message");
-
-const modalContact = document.getElementById("modal-contact");
-const modalContactLoader = document.getElementById("modal-contact-loader");
-const modalContactSuccess = document.getElementById("modal-contact-success");
-const modalContactError = document.getElementById("modal-contact-error");
-const closeModalContact = document.querySelectorAll(".close-modal-contact");
-
-/* Regular Expressions */
-const expName = /^[\S][\DÀ-ÿ\s]{1,30}$/;
-const expEmail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{4,63}\.){1,125}(com|net|org|edu|gov|mil|biz|info|mobi|name|aero|asia|jobs|museum)\b$/i;
-const expMessage = /^[\S][0-9a-zA-ZÀ-ÿ\s\,.¡!¿?]{3,1000}$/;
-
-const campos = {
-  nombre: false,
-  correo: false,
-  mensaje: false
-}
-
-/* Validate Regular Expressions */
-function validateRegularExpressions(e) {
-  switch (e.target.id) { // with e.target.name we get the name value of each input
-
-    case "input-name":
-      // test if the regular expression nombre is true in the input value (e.target.value)
-      if (expName.test(e.target.value)) {
-        alertName.classList.remove('active');
-        campos.nombre = true;
-      }
-      else {
-        alertName.classList.add('active');
-        campos.nombre = false;
-      }
-      break;
-
-    case "input-email":
-      // test if the regular expression correo is true in the input value (e.target.value)
-      if (expEmail.test(e.target.value)) {
-        alertEmail.classList.remove('active');
-        campos.correo = true;
-      }
-      else {
-        alertEmail.classList.add('active');
-        campos.correo = false;
-      }
-      break;
-
-    case "textarea":
-      // test if the regular expression mensaje is true in the input value (e.target.value)
-      if (expMessage.test(e.target.value)) {
-        alertMessage.classList.remove('active');
-        campos.mensaje = true;
-      }
-      else {
-        alertMessage.classList.add('active');
-        campos.mensaje = false;
-      }
-      break;
-  }
-}
-
-/* Replace white spaces on the input email */
-inputEmail.addEventListener("input", () => {
-  inputEmail.value = inputEmail.value.replace(/ /g, "");
-})
-
-/* Inputs events */
-inputs.forEach((input) => {
-  input.addEventListener('blur', validateRegularExpressions);
-  input.addEventListener('input', validateRegularExpressions);
-});
-
-/* Textarea events */
-textarea.addEventListener('blur', validateRegularExpressions);
-textarea.addEventListener('input', validateRegularExpressions);
-
-/* Submit */
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  if (!campos.nombre) {
-    alertName.classList.add('active');
-    location.href = "#contact";
   }
 
-  if (!campos.correo) {
-    alertEmail.classList.add('active');
-    location.href = "#contact";
-  }
+  /* Replace white spaces on the input email */
+  inputEmail.addEventListener("input", () => {
+    inputEmail.value = inputEmail.value.replace(/ /g, "");
+  })
 
-  if (!campos.mensaje) {
-    alertMessage.classList.add('active');
-    location.href = "#contact";
-  }
+  /* Inputs events */
+  inputs.forEach((input) => {
+    input.addEventListener('blur', validateRegularExpressions);
+    input.addEventListener('input', validateRegularExpressions);
+  });
 
-  if (campos.nombre && campos.correo && campos.mensaje) {
+  /* Textarea events */
+  textarea.addEventListener('blur', validateRegularExpressions);
+  textarea.addEventListener('input', validateRegularExpressions);
 
-    modalContact.classList.add("active");
+  /* Submit */
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-    emailjs.sendForm('service_jr41wt8', 'template_m5qbqbi', '#form', '24LpnDak6PKXwQT2O').then(() => {
-      setTimeout(() => {
-        modalContactLoader.style.display = "none";
-        modalContactSuccess.style.display = "block";
-        form.reset();
-      }, 3000);
-    },
+    if (!campos.nombre) {
+      alertName.classList.add('active');
+      location.href = "#contact";
+    }
 
-      (error) => {
+    if (!campos.correo) {
+      alertEmail.classList.add('active');
+      location.href = "#contact";
+    }
+
+    if (!campos.mensaje) {
+      alertMessage.classList.add('active');
+      location.href = "#contact";
+    }
+
+    if (campos.nombre && campos.correo && campos.mensaje) {
+
+      modalContact.classList.add("active");
+
+      emailjs.sendForm('service_jr41wt8', 'template_m5qbqbi', '#form', '24LpnDak6PKXwQT2O').then(() => {
         setTimeout(() => {
           modalContactLoader.style.display = "none";
-          modalContactError.style.display = "block";
+          modalContactSuccess.style.display = "block";
           form.reset();
         }, 3000);
-        console.log(error);
-      });
-  }
-});
+      },
 
-////////// CLOSE MODAL CONTACT //////////
-closeModalContact.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    btn.classList.remove("active");
-    location.href = "index.html"
+        (error) => {
+          setTimeout(() => {
+            modalContactLoader.style.display = "none";
+            modalContactError.style.display = "block";
+            form.reset();
+          }, 3000);
+          console.log(error);
+        });
+    }
   });
+
+  ////////// CLOSE MODAL CONTACT //////////
+  closeModalContact.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      btn.classList.remove("active");
+      location.href = "index.html"
+    });
+  })
+
 });
